d207657f21016064f7159561467f18e5ac72118f
mdrdani <sudokill3004@gmail.com>
2023-01-27 14:01:19 +0700

    push app golang

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	hostname, _ := os.Hostname()
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"code":    200,
+			"message": "api server",
+			"server":  hostname,
+		})
+	})
+	r.Run("0.0.0.0:8144")
+}
