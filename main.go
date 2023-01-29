d469daa16e9886a14fae2ce35656b8b44079066c
mdrdani <sudokill3004@gmail.com>
2023-01-29 22:37:48 +0700

    fix main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 			"code":    200,
 			"message": "api server",
 			"server":  hostname,
-			"author": "M Dani Ramanda"
+			"author": "M Dani Ramanda",
 		})
 	})
 	r.Run("0.0.0.0:8144")
